e59a2f2c2a148ff7c6ebbf44f98fb61e6a77b7f6
Abdoulaye Dong <abdoulayedong@gmail.com>
2021-09-02 12:31:47 +0100

    Complete splash screen

---
+++ AnimatedSplashScreen/AnimatedSplashScreen.Android/SplashActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Airbnb.Lottie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnimatedSplashScreen.Droid
+{
+    [Activity(Label = "SplashActivity",MainLauncher =true,
+        NoHistory =true, Theme = "@style/SplashTheme")]
+    public class SplashActivity : Activity, Animator.IAnimatorListener
+    {
+        public void OnAnimationCancel(Animator animation)
+        {
+           
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+            
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+           
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Activity_Splah);
+            var animationView = FindViewById<LottieAnimationView>(Resource.Id.animation_view);
+            animationView.AddAnimatorListener(this);
+        }
+
+    }
+}
